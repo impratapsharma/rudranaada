baa980225901a66310334c4902554e76a7db75a6
impratapsharma <impratapsharma@gmail.com>
2026-09-22 19:28:36 +0530

    Move Diwali cluster out of generic articles

--- lib/research-articles.ts
+++ lib/research-articles.ts
@@ -1,7 +1,6 @@
 import type {Article} from './content';
-import {diwaliArticles} from './diwali-articles';
 
-const coreResearchArticles:Article[]=[
+export const researchArticles:Article[]=[
   {
     slug:'navratri-2026-nine-forms-of-durga',
     title:'Navratri 2026: Nine Nights, Nine Forms, and What We Are Really Worshipping',
@@ -409,6 +408,3 @@ const coreResearchArticles:Article[]=[
     ]
   }
 ];
-
-
-export const researchArticles:Article[]=[...diwaliArticles,...coreResearchArticles];
